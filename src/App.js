c509c633d62a0dd1237828fb0b122f84eb3ccc39
Roms <roms10885@yandex.ru>
2022-11-21 11:44:34 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -2,16 +2,12 @@ import './App.css';
 import MapComponent from "./components/MapComponent";
 import {useEffect} from "react";
 
-const tg = window.Telegram.WebApp
+const tg= window.Telegram.WebApp
 
 function App() {
-  useEffect(()=>{
-    tg.ready()
-  },[])
+  useEffect(()=> tg.ready(), [])
 
-  const onClose=()=> {
-    tg.close()
-  }
+  console.log('@@@@@ fdsaf',)
   return (
     <div className="App">
      <MapComponent />
